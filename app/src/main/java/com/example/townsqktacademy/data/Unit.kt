a78863ac0543b0205aa2e301240d708e124ca723
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-05-24 11:06:43 -0300

    Creating new data class and adapter to hold card values.

---
+++ app/src/main/java/com/example/townsqktacademy/data/Unit.kt
@@ -0,0 +1,8 @@
+package com.example.townsqktacademy.data
+
+data class Unit(
+    val id: String,
+    val title: String,
+    val description: String,
+    val imageSource: String,
+)
---
+++ app/src/main/java/com/example/townsqktacademy/ui/components/UnitAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.townsqktacademy.ui.components
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.townsqktacademy.R
+import com.example.townsqktacademy.data.Unit
+
+class UnitAdapter() : RecyclerView.Adapter<UnitAdapter.UnitViewHolder>(){
+
+    private var _dataList: MutableList<Unit> = mutableListOf()
+
+    var dataList: List<Unit> = _dataList.toList()
+
+    fun setData(units: List<Unit>) {
+        this._dataList = units.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.unit_card, parent, false)
+        return UnitViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UnitViewHolder, position: Int) {
+        holder.bind(_dataList[position])
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    inner class UnitViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        private var unit: Unit? = null
+
+        private val titleTextView: TextView = view.findViewById(R.id.titleTextView)
+        private val descriptionTextView: TextView = view.findViewById(R.id.descriptionTextView)
+        private val unitImageView: ImageView = view.findViewById(R.id.unitImageView)
+
+        fun bind(unit: Unit) {
+            this.unit = unit
+
+            titleTextView.text = unit.title
+            descriptionTextView.text = unit.description
+            unitImageView.load(unit.imageSource)
+        }
+
+    }
+}
